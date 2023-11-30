be6f79d51adc9fc4305f7737de831007d0177476
SchulZuani <152494337+SchulZuani@users.noreply.github.com>
2023-11-30 16:44:45 +0100

    remote change

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Git is cool. Really ? YESS");
-Console.WriteLine("Some Text m1 f1");
+Console.WriteLine("Some Text m1 f1 remote1");
